2e0139a1169f7cd6d49cc815ddeff2d6c1a622ff
Jessica Sumner <jess.r.sumner@gmail.com>
2015-11-11 22:44:22 -0500

    Initial commit
    Rails version and Ruby version set to match tutorial. Gems strong parameters and monban installed. Bundle complete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = 'c224939eec0078d30a8511d6d03297b2b5c1e23a7d48d626b70380977f3724b660dc578e68f34a9e261fe0f9157bbe9948150a43e31c3c38ba0eb21678b5425b'
